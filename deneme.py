499f914b82f0487aec3e59eb5a44164f310e922a
Veysel <veyselerkoyuncu27@gmail.com>
2023-10-31 13:00:04 +0300

    big update and cleaning unless files

--- deneme.py
+++
@@ -1,3 +0,0 @@
-metin = "siirt"
-metin_buyuk = metin.title()
-print(metin_buyuk)  # Çıktı: "SİİRT"
--- veriDuzeltme.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-df = pd.read_csv('ilcelerMesafeDonusturulmus.csv')
-df['Toplam Uzunluk(km)'] = df['Toplam Uzunluk(km)'].astype(int)
-print("dönüşüm tamamlandı")
-df.to_csv('donusmus_veri.csv', index=False)

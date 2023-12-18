277b41f80baf5487cd727da622ce18dd1511e6ce
Veysel <veyselerkoyuncu27@gmail.com>
2023-12-18 11:40:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-﻿# -*- coding: utf-8 -*-
-import pandas as pd
+﻿import pandas as pd
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from urllib.parse import unquote
 
 app = FastAPI(title="Mesafe")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=['*']
 )
-
 # Türkçe karakterleri büyük harfe çevirmek için bir işlev
 def turkce_buyuk_harf_cevir(metin):
     harf_cevir = {
@@ -21,10 +18,10 @@ def turkce_buyuk_harf_cevir(metin):
         'ö': 'Ö',
         'ç': 'Ç'
     }
+
     return ''.join(harf_cevir.get(harf, harf.upper()) for harf in metin)
 
 df = pd.read_csv("ilcelerDonusturuldu_kucukharf.csv")
-
 @app.get('/mesafe/{k_il}/{k_ilce}/{v_il}/{v_ilce}')
 def mesafeGetir(k_il: str, k_ilce: str, v_il: str, v_ilce: str):
     try:
@@ -33,18 +30,12 @@ def mesafeGetir(k_il: str, k_ilce: str, v_il: str, v_ilce: str):
         k_ilce = turkce_buyuk_harf_cevir(k_ilce)
         v_il = turkce_buyuk_harf_cevir(v_il)
         v_ilce = turkce_buyuk_harf_cevir(v_ilce)
-
         veri = df[(df['k_il'] == k_il) & (df['k_ilce'] == k_ilce) & 
                   (df['v_il'] == v_il) & (df['v_ilce'] == v_ilce)]
         mesafe = veri["Toplam Uzunluk(km)"].sum()
 
-        # Mesafeyi metin olarak biçimlendir ve URL kodlamasını çöz
+        # Mesafeyi metin olarak biçimlendir
         mesafe_metni = f"{k_il} İLİNİN {k_ilce} İLÇESİNDEN {v_il} İLİNİN {v_ilce} İLÇESİNE OLAN MESAFE: {int(mesafe)} KM"
-        mesafe_metni = unquote(mesafe_metni)
-        
-        # Unicode karakterleri düzelt
-        mesafe_metni = mesafe_metni.encode('latin-1').decode('utf-8')
-        
         return {"mesafe": mesafe_metni}
     except Exception as e:
         return {"error": str(e)}

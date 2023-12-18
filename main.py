b19e63480796fbecf0f01bea1b6fe1b975a0462e
Veysel <veyselerkoyuncu27@gmail.com>
2023-12-18 11:44:43 +0300

    veri seti değişim
    veri setindeki veriler büyük hartften küçük harfe değiştirildi

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-﻿import pandas as pd
+﻿#-*- coding: utf-8 -*-
+import pandas as pd
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from urllib.parse import unquote
 
 app = FastAPI(title="Mesafe")
 app.add_middleware(
@@ -26,16 +28,16 @@ df = pd.read_csv("ilcelerDonusturuldu_kucukharf.csv")
 def mesafeGetir(k_il: str, k_ilce: str, v_il: str, v_ilce: str):
     try:
         # Girişleri büyük harfe çevirin
-        k_il = turkce_buyuk_harf_cevir(k_il)
-        k_ilce = turkce_buyuk_harf_cevir(k_ilce)
-        v_il = turkce_buyuk_harf_cevir(v_il)
-        v_ilce = turkce_buyuk_harf_cevir(v_ilce)
+        #k_il = turkce_buyuk_harf_cevir(k_il)
+        #k_ilce = turkce_buyuk_harf_cevir(k_ilce)
+        #v_il = turkce_buyuk_harf_cevir(v_il)
+        #v_ilce = turkce_buyuk_harf_cevir(v_ilce)
         veri = df[(df['k_il'] == k_il) & (df['k_ilce'] == k_ilce) & 
                   (df['v_il'] == v_il) & (df['v_ilce'] == v_ilce)]
         mesafe = veri["Toplam Uzunluk(km)"].sum()
 
         # Mesafeyi metin olarak biçimlendir
-        mesafe_metni = f"{k_il} İLİNİN {k_ilce} İLÇESİNDEN {v_il} İLİNİN {v_ilce} İLÇESİNE OLAN MESAFE: {int(mesafe)} KM"
+        mesafe_metni = f"{k_il.upper()} İLİNİN {k_ilce.upper()} İLÇESİNDEN {v_il.upper()} İLİNİN {v_ilce.upper()} İLÇESİNE OLAN MESAFE: {int(mesafe)} KM"
         return {"mesafe": mesafe_metni}
     except Exception as e:
         return {"error": str(e)}
